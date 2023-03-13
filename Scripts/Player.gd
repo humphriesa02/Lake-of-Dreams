4d3646de283dca7739ada674e488dc7abde879da
Hero <humphriesa02@gmail.com>
2023-03-12 22:55:17 -0700

    Animations and tileset

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,7 @@ var input_direction = Vector2.ZERO
 
 @onready var animation_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 
 func _unhandled_input(event):
@@ -15,15 +16,18 @@ func _unhandled_input(event):
 	input_direction = Input.get_vector("left", "right", "up", "down").normalized()
 	
 func _physics_process(delta):
+	print(animation_tree.get("parameters/Idle/blend_position"))
 	# If we have input
 	if input_direction != Vector2.ZERO:
 		print(input_direction)
+		
 		animation_tree.set("parameters/Idle/blend_position", input_direction)
 		animation_tree.set("parameters/Walk/blend_position", input_direction)
+		animation_state.travel("Walk")
 		# Have our velocity approach our speed in the direction of our input, using acceleration
 		velocity = velocity.move_toward(input_direction * speed, accel * delta)
 	else:
-		animation_player.play("IdleRight")
+		animation_state.travel("Idle")
 		# Have velocity approach zero using the friction variable
 		velocity = velocity.move_toward(Vector2.ZERO, frict * delta)
 	move_and_slide()
